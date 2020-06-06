518346d9e204ad6959ef5e78122888584aea6122
falakh <zulfikarfahmi38@gmail.com>
2020-06-06 10:22:38 +0700

    Updates

---
+++ precache-manifest.037f2ff988722e6da7a284bda981abb0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c22d8f52a82b3dfbeec4b9fdc4182fb9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7fa73c0e69cf008bff1",
+    "url": "/static/css/main.834e6cca.chunk.css"
+  },
+  {
+    "revision": "b11fa79bc8bd1644836e",
+    "url": "/static/js/2.fb75cd9f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.fb75cd9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7fa73c0e69cf008bff1",
+    "url": "/static/js/main.46bcebdc.chunk.js"
+  },
+  {
+    "revision": "964e2b4baceaf86b3515",
+    "url": "/static/js/runtime-main.d7b4ca07.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
